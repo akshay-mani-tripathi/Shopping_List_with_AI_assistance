fd58af7b4e195af8302985828fb02d9172fba141
Akshay mani tripathi <152083530+akshay-mani-tripathi@users.noreply.github.com>
2025-08-31 20:52:53 +0530

    Update api.jsx

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const sendVoiceCommand = async (text) => {
-  const res = await axios.post('http://localhost:3000/voice-command', { text });
+  const res = await axios.post('https://shopping-list-with-ai-assistance.onrender.com/voice-command', { text });
   return res.data;
 };
